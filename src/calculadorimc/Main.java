c70bfd3ee7f72ba38abd8fc610c760b52d373f0b
PaulSebastianNaspud <naspud972012@gmaail.com>
2024-11-13 10:42:03 -0500

    finalizado

--- src/calculadorimc/Main.java
+++ src/calculadorimc/Main.java
@@ -16,7 +16,7 @@ public class Main extends javax.swing.JFrame {
     public Main() {
         initComponents();
     }
-
+    boolean cambiarSis = false;
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -37,10 +37,10 @@ public class Main extends javax.swing.JFrame {
         estatura = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         peso = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
+        jAltura1 = new javax.swing.JLabel();
+        jPeso1 = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
-        cambiarSis = new javax.swing.JButton();
+        ingles = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
@@ -49,6 +49,7 @@ public class Main extends javax.swing.JFrame {
         jLabel12 = new javax.swing.JLabel();
         jLabel13 = new javax.swing.JLabel();
         jLabel14 = new javax.swing.JLabel();
+        metrico = new javax.swing.JButton();
         calcular = new javax.swing.JButton();
         jLabel15 = new javax.swing.JLabel();
         jIMC = new javax.swing.JLabel();
@@ -112,14 +113,19 @@ public class Main extends javax.swing.JFrame {
 
         peso.setBackground(new java.awt.Color(204, 204, 204));
 
-        jLabel5.setText("Metros");
+        jAltura1.setText("Metros");
 
-        jLabel6.setText("Kilogramos");
+        jPeso1.setText("Kilogramos");
 
         jPanel4.setBackground(new java.awt.Color(204, 204, 204));
         jPanel4.setForeground(new java.awt.Color(0, 0, 0));
 
-        cambiarSis.setText("CAMBIAR SISTEMA ");
+        ingles.setText("S. INGLES");
+        ingles.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                inglesActionPerformed(evt);
+            }
+        });
 
         jLabel7.setForeground(new java.awt.Color(0, 0, 0));
         jLabel7.setText("SISTEMA METRICO :");
@@ -137,13 +143,20 @@ public class Main extends javax.swing.JFrame {
         jLabel11.setText("Estatura: ");
 
         jLabel12.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel12.setText("Pies / Pulg");
+        jLabel12.setText("Pulg");
 
         jLabel13.setForeground(new java.awt.Color(0, 0, 0));
         jLabel13.setText("Peso:");
 
         jLabel14.setForeground(new java.awt.Color(0, 0, 0));
         jLabel14.setText("Libras");
+
+        metrico.setText("S. METRICO");
+        metrico.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                metricoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
@@ -157,10 +170,6 @@ public class Main extends javax.swing.JFrame {
                         .addGap(6, 6, 6)
                         .addComponent(jLabel10)))
                 .addGap(16, 16, 16))
-            .addGroup(jPanel4Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(cambiarSis)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addGap(18, 18, 18)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -180,7 +189,12 @@ public class Main extends javax.swing.JFrame {
                     .addGroup(jPanel4Layout.createSequentialGroup()
                         .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel8)
-                            .addComponent(jLabel9))
+                            .addComponent(jLabel9)
+                            .addGroup(jPanel4Layout.createSequentialGroup()
+                                .addGap(6, 6, 6)
+                                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(metrico, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(ingles, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                         .addGap(0, 0, Short.MAX_VALUE))))
         );
         jPanel4Layout.setVerticalGroup(
@@ -202,9 +216,11 @@ public class Main extends javax.swing.JFrame {
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel14)
                     .addComponent(jLabel13))
-                .addGap(18, 18, 18)
-                .addComponent(cambiarSis)
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(ingles)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(metrico)
+                .addContainerGap(9, Short.MAX_VALUE))
         );
 
         calcular.setBackground(new java.awt.Color(0, 51, 255));
@@ -247,13 +263,13 @@ public class Main extends javax.swing.JFrame {
                                     .addComponent(jIMC, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addGap(18, 18, 18)
                                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel6)
-                                    .addComponent(jLabel5)))
+                                    .addComponent(jPeso1)
+                                    .addComponent(jAltura1)))
                             .addGroup(jPanel3Layout.createSequentialGroup()
                                 .addGap(34, 34, 34)
                                 .addComponent(jResultado)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                    .addGroup(jPanel3Layout.createSequentialGroup()
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(calcular)
                         .addGap(134, 134, 134)))
@@ -263,32 +279,31 @@ public class Main extends javax.swing.JFrame {
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addGap(14, 14, 14)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
-                            .addComponent(estatura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel5))
-                        .addGap(11, 11, 11)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel4)
-                            .addComponent(peso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel6))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(calcular)
-                        .addGap(21, 21, 21)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel15)
-                            .addComponent(jIMC))
-                        .addGap(18, 18, 18)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel16)
-                            .addComponent(jResultado)))
-                    .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(21, Short.MAX_VALUE))
+                .addGap(14, 14, 14)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(estatura, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jAltura1))
+                .addGap(11, 11, 11)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(peso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPeso1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(calcular)
+                .addGap(21, 21, 21)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel15)
+                    .addComponent(jIMC))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel16)
+                    .addComponent(jResultado))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -323,26 +338,77 @@ public class Main extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void calcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_calcularActionPerformed
-        //Conversiones y Calculo
-        String pesoString = peso.getText();
-        String alturaString = estatura.getText();
-        double peso = Double.parseDouble(pesoString);
-        double altura = Double.parseDouble(alturaString);
-        double imcR = peso/(altura*altura);
-        String imcRString = String.valueOf(imcR);
-        //Evaluar Resultados
-        jIMC.setText(imcRString);
-        if(imcR <= 18.5){
-            jResultado.setText("Bajo Peso");
-        }else if (imcR > 18.5 && imcR <= 24.9 ){
-             jResultado.setText("Peso saludable");
-        }else if (imcR >= 25 && imcR <= 29.9 ) {
-             jResultado.setText("Sobrepeso");
-        } else if (imcR >=  30 ){
-             jResultado.setText("Obesidad");
+       
+        if (cambiarSis = false){
+             //Conversiones y Calculo
+            String pesoString = peso.getText();
+            String alturaString = estatura.getText();
+            double pesoDouble = Double.parseDouble(pesoString);
+            double alturaDouble = Double.parseDouble(alturaString);
+            double imcR = pesoDouble/(alturaDouble*alturaDouble);
+            String imcRString = String.valueOf(imcR);
+            //Evaluar Resultados
+            jIMC.setText(imcRString);
+            if(imcR <= 18.5 && imcR > 0){
+                 jResultado.setText("Bajo Peso");
+            }else if (imcR > 18.5 && imcR <= 24.9 ){
+                jResultado.setText("Peso saludable");
+            }else if (imcR >= 25 && imcR <= 29.9 ) {
+                jResultado.setText("Sobrepeso");
+             } else if (imcR >=  30 ){
+                jResultado.setText("Obesidad");
+             } else {
+                 jResultado.setText("Volver a calcular");
+                 jIMC.setText("Datos Invalidos");
+                 peso.setText("");
+                 estatura.setText("");
+        } 
+        } else {
+               //Conversiones y Calculo
+               String pesoString = peso.getText();
+               String alturaString = estatura.getText();  
+               double pesoDouble = Double.parseDouble(pesoString);
+               double alturaDouble = Double.parseDouble(alturaString);
+               double imcR = pesoDouble/(alturaDouble*alturaDouble)* 703;
+               String imcRString = String.valueOf(imcR);
+                //Evaluar Resultados
+               jIMC.setText(imcRString);
+               if(imcR <= 18.5 && imcR > 0){
+                 jResultado.setText("Bajo Peso");
+               }else if (imcR > 18.5 && imcR <= 24.9 ){
+                jResultado.setText("Peso saludable");
+               }else if (imcR >= 25 && imcR <= 29.9 ) {
+                jResultado.setText("Sobrepeso");
+               } else if (imcR >=  30 ){
+                jResultado.setText("Obesidad");
+               } else {
+                 jResultado.setText("Volver a calcular");
+                 jIMC.setText("Datos Invalidos");
+                 peso.setText("");
+                 estatura.setText("");
+        } 
+               
         }
         
+        
     }//GEN-LAST:event_calcularActionPerformed
+
+    private void inglesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inglesActionPerformed
+        // Cambiar Labels
+        jAltura1.setText("Pulg");
+        jPeso1.setText("Libras");
+        cambiarTit.setText("INGLES");
+        cambiarSis = true;
+        
+    }//GEN-LAST:event_inglesActionPerformed
+
+    private void metricoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_metricoActionPerformed
+        // TODO add your handling code here:
+        jAltura1.setText("Metros");
+        jPeso1.setText("Kilogramos");
+        cambiarTit.setText("METRICO");
+        cambiarSis = false;
+    }//GEN-LAST:event_metricoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -381,9 +447,10 @@ public class Main extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton calcular;
-    private javax.swing.JButton cambiarSis;
     private javax.swing.JLabel cambiarTit;
     private javax.swing.JTextField estatura;
+    private javax.swing.JButton ingles;
+    private javax.swing.JLabel jAltura1;
     private javax.swing.JLabel jIMC;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
@@ -396,8 +463,6 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
@@ -405,8 +470,10 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
+    private javax.swing.JLabel jPeso1;
     private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem1;
     private javax.swing.JLabel jResultado;
+    private javax.swing.JButton metrico;
     private javax.swing.JTextField peso;
     // End of variables declaration//GEN-END:variables
 }
